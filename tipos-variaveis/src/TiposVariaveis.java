f93f67f48dcfd0bf71a821fa1b815d19d722e80d
doni63 <dbatistajunior63@gmail.com>
2025-03-02 15:38:51 -0300

    testando boolean

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -5,7 +5,17 @@ public class TiposVariaveis {
 
         final double PI = 3.14;
 
-        System.out.println(numero1);
-        System.out.println(PI);
+      //  System.out.println(numero1);
+      //System.out.println(PI);
+
+      int numero2 = 0;
+
+      int resultado = numero1 + numero2;
+
+      boolean teste = resultado == 12;
+
+      System.out.println("O valor de resultado é 2: " + teste);
+
+
     }
 }
