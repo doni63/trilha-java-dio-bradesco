d788e37f91ca497d6c1124c95eef69267fe7fa36
doni63 <dbatistajunior63@gmail.com>
2025-03-03 11:11:17 -0300

    criando metodos aumentar e diminuir volume, ligar e desligar tv.

---
+++ exercitando/src/Principal.java
@@ -0,0 +1,25 @@
+public class Principal {
+    public static void main(String[] args) throws Exception {
+        
+
+        SmartTv tv = new SmartTv();
+        tv.setLigada(false);
+        tv.setVolume(10);
+        tv.setCanal(5);
+
+      
+       /* tv.aumentarVolume();
+       System.out.println("Volume aumentou para " + tv.getVolume());
+       tv.diminuirVolume();
+       System.out.println("Volume diminuiu para " + tv.getVolume());
+       tv.aumentarCanal();
+       System.out.println("Canal aumentou para " + tv.getCanal());
+       tv.diminuirCanal();
+       System.out.println("Canal diminuiu para " + tv.getCanal());
+       */
+
+       tv.ligarTv();
+       tv.desligarTv();
+
+    }
+}
---
+++ exercitando/src/SmartTv.java
@@ -0,0 +1,74 @@
+public class SmartTv {
+    private int canal;
+    private int volume;
+    private boolean ligada;
+
+    public SmartTv(int canal, int volume, boolean ligada) {
+        this.canal = canal;
+        this.volume = volume;
+        this.ligada = ligada;
+    }
+
+    public SmartTv() {
+
+    }
+
+    public int getCanal() {
+        return canal;
+    }
+
+    public void setCanal(int canal) {
+        this.canal = canal;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public boolean isLigada() {
+        return ligada;
+    }
+
+    public void setLigada(boolean ligada) {
+        this.ligada = ligada;
+    }
+    
+    public void aumentarVolume() {
+        volume++;
+    }
+
+    public void diminuirVolume() {
+        volume--;
+    }
+
+    public void aumentarCanal() {
+        canal++;
+    }
+
+    public void diminuirCanal() {
+        canal--;
+    }
+
+    public void ligarTv() {
+        if (ligada == false) {
+            ligada = true;
+        } else if (ligada == true) {
+            ligada = true;
+        }
+        System.out.println("Tv ligada!");
+    }
+
+    public void desligarTv() {
+        if (ligada ) {
+            ligada = false;
+        } else if (!ligada) {
+            ligada = false;
+        }
+        System.out.println("Tv desligada!");
+    }
+
+}
